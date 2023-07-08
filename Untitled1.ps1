7114e4887c9ea356a877538fbcfd7031f6440227
marufke1 <137698120+marufke1@users.noreply.github.com>
2023-07-08 16:14:51 -0400

    Add files via upload
    Description:
    This Power shell scripts allows to automate different important information related our  system and need to execute those command for daily activities.
    1.  BIOS DETAILS
    2. SYSTEM PROCESSOR
    3. RUNNING SERVICES IN THE SYTEM
    4. USER LOGGED INFO
    5. DISK SPACE CAPACITY LEFT
    6. O/S TYOPE FOR THE SYSTEM

---
+++ Untitled1.ps1
@@ -0,0 +1,11 @@
+﻿Write-Host "Welcome to the script of fetching computer information"
+Write-Host "The Bios details are as follows" Get-CimInstance - ClassName Win32_Bios
+
+Write_Host "The System processor is" Get-CimInstance - ClassName Win32-ComputerSystem | Select-Object - Property Systemtype
+
+Write-Host "Status of the running services are as follows" Get-CimInstance - ClassName Win32_Service | Format-table -Property Status,Name,Displayname -Autosize -wrap
+Write-Host "Current user logged in to the system" Get-CimInstance - ClassName Win32_computerSystem - Property Username
+Write-Host "The disk sapce details are as follows" Get-CimInstance - ClassNAme Win32_LogicalDisk - Filter "Drivetype=3" |Measure-Object -Property Freespace,
+size -Sum |Select-Object -Property Property,Sum
+
+Write-Host "The os details aree below" Get-Ciminstance - ClassNAme Win32_Operating Sytem |Select-Object - Property Build*,OSTypexServicePAck*
